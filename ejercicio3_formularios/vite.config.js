a142d6c8c5a0bba6577596a3ed71f71794174d39
FelipeKlinger <juanfelipetaborda9@gmail.com>
2025-11-13 12:39:04 +0100

    Front para Render

--- ejercicio3_formularios/vite.config.js
+++ ejercicio3_formularios/vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: { // añadedo configuración del proxy
+    proxy: {
+      '/api': { // cualquier llamada que empiece por /api se redirige
+        target: 'http://localhost:3001', // al servidor de node
+        changeOrigin: true, // para evitar problemas de CORS
+      },
+    }
+  },
 })
